7ff90e884be84a600133d4959c01880ed8d22da4
valentixcrupkin <valentinacrupkin03@gmail.com>
2021-10-01 17:07:56 -0300

    firebase 2

--- firebase-ej1/main.js
+++ firebase-ej1/main.js
@@ -49,7 +49,8 @@ const listar_clientes = async () => {
                 <td>${cliente.apellido}</td>
                 <td>${cliente.dni}</td>
                 <td>
-                    <button class="btn btn-danger btn-sm">x</button>
+                    <button onclick="eliminar_cliente('${cliente.id}')" class="btn btn-danger btn-sm"><i class="fa fa-trash"></i></button>
+                    <button onclick="llenar_formulario('${cliente.nombre}','${cliente.apellido}',${cliente.dni},'${cliente.id}')" class="btn btn-primary btn-sm">Editar</button>
                 </td>
             </tr>
         `
@@ -62,3 +63,45 @@ const listar_clientes = async () => {
 
 
 listar_clientes()
+
+
+
+function eliminar_cliente(id)
+{
+    db.collection("cliente").doc(id).delete()
+    listar_clientes()
+}
+
+
+function llenar_formulario(nom,ape,dni,id)
+{
+    document.getElementById("inp_nom").value = nom;
+    document.getElementById("inp_ape").value = ape;
+    document.getElementById("inp_dni").value = dni;
+    document.getElementById("inp_id").value = id;
+
+    document.getElementById("btn_guardar").style.display = 'none';
+    document.getElementById("btn_actualizar").style.display = 'block';
+}
+
+function actualizar_cliente()
+{
+    const nom = document.getElementById("inp_nom").value
+    const ape = document.getElementById("inp_ape").value
+    const dni = document.getElementById("inp_dni").value
+    const id = document.getElementById("inp_id").value
+
+    const clienteActualizado = {
+        nombre:nom,
+        apellido:ape,
+        dni:dni,
+    }
+
+    db.collection("cliente").doc(id).update(clienteActualizado)
+
+    listar_clientes()
+    vaciar()
+
+    document.getElementById("btn_guardar").style.display = 'block';
+    document.getElementById("btn_actualizar").style.display = 'none';
+}
